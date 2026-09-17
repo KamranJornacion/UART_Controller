ebbece96b286b7b852c9346aad4e8ace831b7bcf
Kamran Jornacion <Kamran.Jornacion@gmail.com>
2026-09-16 22:18:55 -0400

    Moularize counter logic

---
+++ rtl/counter.v
@@ -0,0 +1,19 @@
+module counter #(parameter LENGTH = 4) (
+    input clk,
+    input ce,
+    input rst,
+    output [LENGTH-1:0] count
+);
+
+    reg [LENGTH-1:0] counter;
+    assign count = counter;
+
+    always @(posedge clk) begin
+        if (rst)begin
+            counter <= 'b0;
+        end else if(ce) begin
+            counter <= counter +1;
+        end 
+    end
+
+endmodule
